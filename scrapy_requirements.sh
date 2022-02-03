2d48f91f562bc0a39dfb5f34430c643d179f1e8c
dwalterjansen <davidwalterjansengithub@gmail.com>
2022-02-03 16:35:09 -0300

    Adiciona requirements do projeto

---
+++ scrapy_requirements.sh
@@ -0,0 +1,4 @@
+#!bin/sh
+
+# See documentation: https://docs.scrapy.org/en/latest/intro/install.html
+sudo apt install python3 python3-dev python3-pip libxml2-dev libxslt1-dev zlib1g-dev libffi-dev libssl-dev
